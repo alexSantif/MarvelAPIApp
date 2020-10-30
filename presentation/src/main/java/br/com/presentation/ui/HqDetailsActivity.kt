ca1a0059eceb4de983fa25088d6df68e4e2c3c27
alexSantosIf <alexsantos2102@gmail.com>
2020-10-29 21:58:40 -0300

    layout improvement

---
+++ presentation/src/main/java/br/com/presentation/ui/HqDetailsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.presentation.R
+
+class HqDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hq_details)
+    }
+}
